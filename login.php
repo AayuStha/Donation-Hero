0997b2e709dd40245322318629f86ffc894df461
Sakshi Maharjan <sakshimaharjan.16310@gmail.com>
2023-12-24 02:07:18 +0545

    header and footer

--- login.php
+++ login.php
@@ -6,6 +6,20 @@
     <title>Document</title>
 </head>
 <body>
+    <nav class="navbar">
+        <a href="/index.html" class="logo">
+            <img src="/photos/logo.jpeg" alt="Logo">
+        </a>
+        <ul class="nav-items">
+            <li><a href="/index.html">Home</a></li>
+            <li><a href="/leaderboard.html">Leaderboard</a></li>
+            <li><a href="/about.html">About</a></li>
+            <li><a href="/events.html">Events</a></li>
+            <li><a href="/contact.html">Contact</a></li>
+            <li><a href="/login.html">Login</a></li>
+            <li><a href="/signup.html">Sign Up</a></li>
+        </ul>
+    </nav>
     <?php
     include 'config.php';
 
@@ -58,5 +72,40 @@
             $mysqli->close();
         } 
         ?>
+        <footer>
+        <div class="footer-container">
+            <div class="footer-column">
+                <h3>Follow Us</h3>
+                <ul>
+                    <li><a href="https://www.facebook.com/" target="_blank">Facebook </a></li>
+                    <li><a href="https://www.twitter.com/" target="_blank">Twitter</a></li>
+                    <li><a href="https://www.instagram.com/" target="_blank">Instagram</a></li>
+                    <li><a href="https://www.linkedin.com/" target="_blank">LinkedIn</a></li>
+                </ul>
+            </div>
+            <div class="footer-column">
+                <h3>Quick Links</h3>
+                <ul>
+                    <li><a href="/about.html">About Us</a></li>
+                    <li><a href="/contact.html">Contact Us</a></li>
+                    <li><a href="/terms.html">Terms & Condition</a></li>
+                    <li><a href="/privacy.html">FAQ</a></li>
+                </ul>
+            </div>
+            <div class="footer-column">
+                <img src="/photos/logo.jpeg" alt="Logo">
+            </div>
+            <div class="footer-column">
+                <h3>Newsletter</h3>
+                <form>
+                    <input type="email" placeholder="Your email address" required>
+                    <button type="submit">Subscribe</button>
+                </form>
+            </div>
+        </div>
+        <div class="copyright">
+            <p>&copy; 2023 DonationHero. All rights reserved.</p>
+        </div>
+    </footer>
 </body>
 </html>
