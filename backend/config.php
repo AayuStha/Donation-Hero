98e4aef7496f84ec029503131fb895e4dbbef807
Shub Raj Lama <hello@shubraj.com>
2023-12-23 17:30:52 +0545

    shubey

---
+++ backend/config.php
@@ -0,0 +1,6 @@
+<?php
+$_servername = "172.130.100.120";
+$_username = "root";
+$_password = "";
+$_database = "donationhero";
+?>
--- backend/login.php
+++ backend/login.php
@@ -7,39 +7,53 @@
 </head>
 <body>
     <?php
+    include 'config.php';
+
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            
             $email = $_POST["email"];
             $password = $_POST["password"];
         
-            $mysqli = new mysqli("localhost", "root", "", "signup");
+            $mysqli = new mysqli($_servername, $_username, $_password, $_database);
         
             if ($mysqli->connect_error) {
                 die("Connection failed: " . $mysqli->connect_error);
             }
         
-            $stmt = $mysqli->prepare("SELECT * FROM Users WHERE email = ?");
+            $stmt = $mysqli->prepare("SELECT id,password FROM Users WHERE email = ?");
             $stmt->bind_param("s", $email);
             $stmt->execute();
+            $stmt->store_result();
+            $num_rows = $stmt->num_rows;
+
+            if ($num_rows > 0) {
+
+                $stmt->bind_result($id,$hashed_password_from_database);
+                $stmt->fetch();
         
-            $result = $stmt->get_result();
-            $user = $result->fetch_assoc();
-        
-            if ($user && password_verify($password, $user['password'])) {
-                // Password is correct, so start a new session
-                session_start();
-                
-                // Store data in session variables
-                $_SESSION["loggedin"] = true;
-                $_SESSION["id"] = $user["id"];
-                $_SESSION["email"] = $email;                            
-                
-                // Redirect user to welcome page
-                header("location: welcome.php");
-            } else {
-                // Display an error message if password is not valid
-                echo "The password you entered was not valid.";
+                if (password_verify($password, $hashed_password_from_database)) {
+
+                    // Password is correct, so start a new session
+                    // $result = $stmt->get_result();
+                    // $user = $result->fetch_assoc();
+                    session_start();
+                    
+                    // // Store data in session variables
+                    $_SESSION["loggedin"] = true;
+                    $_SESSION["user"] = $id;                         
+                    
+                    // Redirect user to welcome page
+                    header("location: welcome.php");
+                } else {
+                    // Display an error message if password is not valid
+                    echo "Invalid username and password combination.";
+                }
+
             }
-        
+            else {
+                // Display an error message if password is not valid
+                echo "Invalid username and password combination.";
+            }
             $stmt->close();
             $mysqli->close();
         } 
